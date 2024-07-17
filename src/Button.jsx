6d74c93a991071a58f6409ce2780b02c68b250e1
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-07-17 11:19:52 +0300

    dqw

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,13 +1,3 @@
 export default function Button() {
-  return <button>Save</button>;
-}
-
-function filter(arr, fn) {
-  const filteredArr = [];
-  for (let i = 0; i < arr.len1gth; i++) {
-    if (fn(arr[i], i)) {
-      filteredArr.push(arr[i]);
-    }
-  }
-  return filteredArr;
+  return <button>dd</button>;
 }
